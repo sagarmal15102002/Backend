3414bfab18416278de98309a8633555c9c8e99fb
sagarmal15102002 <sagarmal15102002@gmail.com>
2025-10-09 09:52:42 -0500

    User and Video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import mongooseAggregatePaginate  from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
 
@@ -9,28 +9,43 @@ const videoSchema = new Schema(
             required: true
         },
         thumbnail: {
-            type: String
+            type: String,
+            required:true
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default:true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref:"User"
         }
-        
- }
 
-
-
-
-
-
-
-
-
-
-
-
+        }
 
     ,{
         timestamps: true
     }
 );
 
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 
 
