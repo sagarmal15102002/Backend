ef9ccdd0cd2f2eaa21885138f6a2ed972fffc98a
sagarmal15102002 <sagarmal15102002@gmail.com>
2025-11-12 10:42:03 -0500

    changes in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,21 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message:"ok"
-    })
+    //   get user details from frontend
+    //   add the validaction
+    // check if user is already exists  through the username and email 
+    // check the images check for avtar
+    // upload them to cloudenary  , avatar
+    // check the response from the cloudinary
+    // create user object - create entry in Db
+    // remove password and refresh_token feilds from the response 
+    // check for user creation 
+    // return res
+    
+    const { fullname, email, username, password } = req.body
+    console.log("email :" , email)
+    
+    
 })
 
 
